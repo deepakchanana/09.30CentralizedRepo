0fd2416b79bcc258b267b43620a3f91f5e2d91ef
deepakchanana <deepakdeep1992@gmail.com>
2020-08-02 10:02:01 -0700

    Pushing the COde

---
+++ Project_1/src/pkg1/Student.java
@@ -0,0 +1,23 @@
+package pkg1;
+
+public class Student 
+{
+  public Student() {
+	// TODO Auto-generated constructor stub
+}
+  public Student(int a) {
+		// TODO Auto-generated constructor stub
+	}
+  public Student(int a,int b) {
+		// TODO Auto-generated constructor stub
+	}
+  
+  public void m()
+  {
+	  
+  }
+   public static void main(String[] args) {
+	
+	 
+}
+}
